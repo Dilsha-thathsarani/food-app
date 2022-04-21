72b39511199f042195c3c1bdeb188834d02f2655
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-04-21 20:50:10 +0530

    HeroSection & CustomeImages

---
+++ src/styles/components/CustomImage.js
@@ -0,0 +1,10 @@
+export default function CustomImage({imgSrc , pt}){
+    return(
+        <div className="custome-image" style={{paddingTop: pt}}>
+
+          <img src={imgSrc} alt="" />
+
+        </div>
+    )
+    
+}
