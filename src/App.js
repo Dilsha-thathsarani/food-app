b36dfdf4a4bfab1348ebaef31c4c6701adaefdcb
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-04-22 08:34:25 +0530

    ImproveSkills

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./styles/components/Navbar";
 import HeroSection from "./styles/components/HeroSection";
+import ImproveSkills from "./styles/components/ImproveSkills";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <div className="container main">
         <HeroSection/>
+        <ImproveSkills/>
       </div>
       
     </div>
