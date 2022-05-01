667ae975f7340c852b84451f9f32a3df8c792cf4
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-05-01 13:32:21 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import Navbar from "./styles/components/Navbar";
-import HeroSection from "./styles/components/HeroSection";
-import ImproveSkills from "./styles/components/ImproveSkills";
-import QuoteSection from "./styles/components/QuoteSection";
-import ChiefsSection from "./styles/components/ChiefsSection";
-//import ChiefCard from "./styles/components/ChiefCard";
+
+import Footer from "./styles/components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="container main">
-        <HeroSection/>
-        <ImproveSkills/>
-        <QuoteSection/>
-        <ChiefsSection />
+        
+       
       </div>
+      <Footer/>
       
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import HeroSection from "./styles/components/HeroSection";
+import ImproveSkills from "./styles/components/ImproveSkills";
+import QuoteSection from "./styles/components/QuoteSection";
+import ChiefsSection from "./styles/components/ChiefsSection";
+
+
+export default function Home(){
+    return(
+        <div>
+
+        <HeroSection/>
+        <ImproveSkills/>
+        <QuoteSection/>
+        <ChiefsSection />
+        </div>
+        
+    )
+}
